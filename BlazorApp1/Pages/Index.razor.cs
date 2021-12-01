02a341b4adb4997aa4c9f99274d88ec33a24ce45
Argo-Tianyi <argo@163.com>
2021-12-01 12:33:53 +0800

    chore: 增加测试工程

---
+++ BlazorApp1/Pages/Index.razor.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Components;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Xml.Linq;
+
+namespace BlazorApp1.Pages
+{
+    public partial class Index
+    {
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            CultureInfo.CurrentUICulture = new CultureInfo("zh-CN");
+            var val = Utility.GetDisplayName(typeof(Dummy), nameof(Dummy.Name));
+        }
+    }
+
+    class Dummy
+    {
+        [Display(Name = "Name1")]
+        public string? Name { get; set; }
+
+        [Display(Name = "Address1")]
+        public string? Address { get; set; }
+    }
+}
---
+++ BlazorApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using BlazorApp1.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddLocalization(option => option.ResourcesPath = "Resources");
+builder.Services.AddBootstrapBlazor(localizationAction: options =>
+{
+    options.ResourceManagerStringLocalizerType = typeof(BlazorApp1.Pages.Index);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
