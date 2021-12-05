058443115b623d395e2dc03604b888d526c1a84d
Argo-Lenovo <argo@163.com>
2021-12-05 18:39:51 +0800

    test: 增加 ShowEditDialog 单元测试

--- test/UnitTest/Components/DialogTest.cs
+++ test/UnitTest/Components/DialogTest.cs
@@ -6,6 +6,7 @@ using BootstrapBlazor.Components;
 using BootstrapBlazor.Shared;
 using Bunit;
 using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
@@ -68,6 +69,7 @@ namespace UnitTest.Components
             }));
             cut.InvokeAsync(() => modal.Instance.Close());
 
+            #region ShowSearchDialog
             // 无按钮回调赋值
             var option = new SearchDialogOption<Foo>()
             {
@@ -104,7 +106,57 @@ namespace UnitTest.Components
             button = cut.FindComponents<Button>().First(b => b.Instance.Text == "查询");
             cut.InvokeAsync(() => button.Instance.OnClickWithoutRender!.Invoke());
             Assert.True(search);
+            #endregion
+
+            #region ShowEditDialog
+            // 无按钮回调赋值
+            var editOption = new EditDialogOption<Foo>()
+            {
+                Model = new Foo(),
+                ItemsPerRow = 2,
+                RowType = RowType.Inline
+            };
+            cut.InvokeAsync(() => dialog.ShowEditDialog(editOption));
             cut.InvokeAsync(() => modal.Instance.Close());
+
+            // 设置关闭回调
+            closed = false;
+            editOption.OnCloseAsync = () =>
+            {
+                closed = true;
+                return Task.CompletedTask;
+            };
+            cut.InvokeAsync(() => dialog.ShowEditDialog(editOption));
+            button = cut.FindComponents<Button>().First(b => b.Instance.Text == "关闭");
+            cut.InvokeAsync(() => button.Instance.OnClickWithoutRender!.Invoke());
+            Assert.True(closed);
+
+            // 设置保存回调
+            var saved = false;
+            editOption.ShowLoading = true;
+            editOption.OnSaveAsync = context =>
+            {
+                saved = true;
+                return Task.FromResult(true);
+            };
+            editOption.Items = new IEditorItem[]
+            {
+                new MockEditorItem()
+            };
+            cut.InvokeAsync(() => dialog.ShowEditDialog(editOption));
+            var form = cut.Find("form");
+            form.Submit();
+            Assert.True(saved);
+
+            // 测试 DialogBodyTemplate
+            editOption.DialogBodyTemplate = foo => builder => builder.AddContent(0, "test");
+            cut.InvokeAsync(() => dialog.ShowEditDialog(editOption));
+            form.Submit();
+            #endregion
+
+            #region ShowModal
+
+            #endregion
         }
 
         private class MockDialogTest : ComponentBase
@@ -112,6 +164,35 @@ namespace UnitTest.Components
             [Inject]
             [NotNull]
             public DialogService? DialogService { get; set; }
+        }
+
+        private class MockEditorItem : IEditorItem
+        {
+            public MockEditorItem()
+            {
+                PropertyType = typeof(string);
+            }
+
+            public System.Type PropertyType { get; }
+            public bool Editable { get; set; }
+            public bool Readonly { get; set; }
+            public bool IsReadonlyWhenAdd { get; set; }
+            public bool IsReadonlyWhenEdit { get; set; }
+            public bool SkipValidate { get; set; }
+            public string? Text { get; set; }
+            public string? PlaceHolder { get; set; }
+            public IEnumerable<SelectedItem>? Items { get; set; }
+            public object? Step { get; set; }
+            public int Rows { get; set; }
+            public RenderFragment<object>? EditTemplate { get; set; }
+            public System.Type? ComponentType { get; set; }
+            public IEnumerable<KeyValuePair<string, object>>? ComponentParameters { get; set; }
+            public IEnumerable<SelectedItem>? Lookup { get; set; }
+            public List<IValidator>? ValidateRules { get; set; }
+
+            public string? GetDisplayName() => Text;
+
+            public string GetFieldName() => Text ?? "Name";
         }
     }
 }
