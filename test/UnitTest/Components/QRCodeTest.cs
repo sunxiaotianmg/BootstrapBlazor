92bb945d25d6efcb645bdac9f1c0cbf9fbc81d1f
zhangpeihang <948869991@qq.com>
2021-12-07 22:38:04 +0800

    test: 添加 QRCode 组件单元测试

--- test/UnitTest/Components/QRCodeTest.cs
+++ test/UnitTest/Components/QRCodeTest.cs
@@ -3,9 +3,11 @@
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
 using BootstrapBlazor.Components;
+using Bunit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using UnitTest.Core;
@@ -66,6 +68,44 @@ namespace UnitTest.Components
             {
                 builder.Add(a => a.Content, "https://www.blazor.zone");
             });
+        }
+
+        [Fact]
+        public void OnGenerated()
+        {
+            var message = "";
+            var cut = Context.RenderComponent<QRCode>(builder =>
+            {
+                builder.Add(a => a.OnGenerated, () => { message = "complete"; return Task.CompletedTask; });
+                builder.Add(a => a.ShowButtons, true);
+            });
+
+            var input = cut.Find("input");
+            input.TextContent = "qrcode";
+
+            var button = cut.Find(".btn-success");
+            button.Click();
+        }
+
+        [Fact]
+        public void Clear()
+        {
+            var cut = Context.RenderComponent<QRCode>(builder =>
+            {
+                builder.Add(a => a.ShowButtons, true);
+            });
+
+            var input = cut.Find("input");
+            input.TextContent = "qrcode";
+
+            var button = cut.Find(".btn-success");
+            button.Click();
+
+            var clearbutton = cut.Find(".btn-secondary");
+            clearbutton.Click();
+
+            var cc = cut.Instance.GetType().GetProperty("MethodName", BindingFlags.Default | BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(cut.Instance, null);
+            //Assert.DoesNotContain("clear",);
         }
     }
 }
