71ef7a8f6fddaace54c8bdc12dc300cd5f3fe979
zhangpeihang <948869991@qq.com>
2021-12-08 20:21:50 +0800

    test: 添加 ColorPicker 组件单元测试

---
+++ test/UnitTest/Components/ColorPickerTest.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Components;
+using Bunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTest.Core;
+using Xunit;
+
+namespace UnitTest.Components
+{
+    public class ColorPickerTest : BootstrapBlazorTestBase
+    {
+        [Fact]
+        public void PlaceHolder_OK()
+        {
+            var cut = Context.RenderComponent<ColorPicker>(builder => builder.Add(a => a.AdditionalAttributes, new Dictionary<string, object>
+            {
+                ["placeholder"] = "Please pick"
+            }));
+
+            Assert.Contains("Please pick", cut.Markup);
+        }
+
+        [Fact]
+        public void DisplayText_OK()
+        {
+            var cut = Context.RenderComponent<ColorPicker>(builder =>
+            {
+                builder.Add(a => a.ShowLabel, true);
+                builder.Add(a => a.DisplayText, "Color picker");
+            });
+
+            Assert.Equal("Color picker", cut.Find("label").TextContent);
+        }
+    }
+}
